f75710fc3445a043a6ae70c4bb9afe54ee052374
Pranjul <pranjulsrivastava0309@gmail.com>
2026-05-31 14:25:34 +0530

    Dynamic goal addition functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
 import "./App.css";
+import GoalCard from "./components/GoalCard.jsx";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [goals, setGoals] = useState(["Learn React", "Fitness"]);
+  const addGoal = () => {
+    setGoals([...goals, "New Goal"]);
+  };
   return (
-    <div>
-      <h1>Welcome Back 🚀</h1>
-      <p>I'm rebuilding my React skills.</p>
+    <div className="app">
+      <h1 className="heading">Goal Tracker</h1>
+
+      <button className="add-goal-btn" onClick={addGoal}>
+        + New Goal
+      </button>
+
+      <div className="goal-list">
+        {goals.map((goal) => (
+          <GoalCard key={goal} title={goal} />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/components/GoalCard.jsx
@@ -0,0 +1,5 @@
+function GoalCard(props) {
+  return <div className="goal-card">{props.title}</div>;
+}
+
+export default GoalCard;
