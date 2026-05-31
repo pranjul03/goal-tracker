d2befc46b190e72680d6a5b70f38d36ee6fd7a1e
Pranjul <pranjulsrivastava0309@gmail.com>
2026-05-31 12:37:58 +0530

    Initial React project setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome Back 🚀</h1>
+      <p>I'm rebuilding my React skills.</p>
+    </div>
+  );
+}
+
+export default App;
